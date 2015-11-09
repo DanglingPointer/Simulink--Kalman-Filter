9854e43ebc1e66b37c4c3c8e3e685c163ff37089
Mikhail Vasilyev <mikail.vasilyev@gmail.com>
2015-11-09 18:13:08 +0100

    KalmanFilter.cpp fixed

--- Src/KalmanFilter.cpp
+++ Src/KalmanFilter.cpp
@@ -13,24 +13,24 @@ using namespace mvkf;
 #define MDL_INITIAL_SIZES
 static void mdlInitializeSizes(SimStruct *S)
 {
-	ssSetNumSFcnParams(S, 0);					// no parameters
+	ssSetNumSFcnParams(S, 0);                   // no parameters
 	if (ssGetNumSFcnParams(S) != ssGetSFcnParamsCount(S))
 		return;	// Parameter mismatch reported by the Simulink engine
 
-	if (!ssSetNumInputPorts(S, 2)) return;		// 2 input ports
-	ssSetInputPortWidth(S, 0, 1);				// input port 0 width = 1
-	ssSetInputPortWidth(S, 1, 1);				// input port 1 width = 1
+	if (!ssSetNumInputPorts(S, 2)) return;      // 2 input ports
+	ssSetInputPortWidth(S, 0, 1);               // input port 0 width = 1
+	ssSetInputPortWidth(S, 1, 1);               // input port 1 width = 1
 
-	ssSetInputPortDirectFeedThrough(S, 0, 1);	// input 0 is used
-	ssSetInputPortDirectFeedThrough(S, 1, 1);	// input 1 is used
+	ssSetInputPortDirectFeedThrough(S, 0, 1);   // input 0 is used
+	ssSetInputPortDirectFeedThrough(S, 1, 1);   // input 1 is used
 
-	if (!ssSetNumOutputPorts(S, 2)) return;		// 2 output ports
-	ssSetOutputPortWidth(S, 0, 1);				// output 0 width = 1
-	ssSetOutputPortWidth(S, 1, 1);				// output 1 width = 1
+	if (!ssSetNumOutputPorts(S, 2)) return;     // 2 output ports
+	ssSetOutputPortWidth(S, 0, 1);              // output 0 width = 1
+	ssSetOutputPortWidth(S, 1, 1);              // output 1 width = 1
 
-	ssSetNumSampleTimes(S, 1);					// 1 block-based sample time
+	ssSetNumSampleTimes(S, 1);                  // 1 block-based sample time
 
-	ssSetNumPWork(S, 2);						// reserve 2 pointers for our objects
+	ssSetNumPWork(S, 2);                        // reserve 2 pointers for our objects
 	
 	ssSetSimStateCompliance(S, USE_CUSTOM_SIM_STATE);
 
@@ -39,8 +39,8 @@ static void mdlInitializeSizes(SimStruct *S)
 #define MDL_INITIALIZE_SAMPLE_TIMES
 static void mdlInitializeSampleTimes(SimStruct *S)
 {
-	ssSetSampleTime(S, 0, 0.1);					// frequency = 10Hz
-	ssSetOffsetTime(S, 0, 0.0);					// no offset
+	ssSetSampleTime(S, 0, 0.1);                 // frequency = 10Hz
+	ssSetOffsetTime(S, 0, 0.0);                 // no offset
 }
 #define MDL_START
 static void mdlStart(SimStruct *S)
@@ -51,10 +51,10 @@ static void mdlStart(SimStruct *S)
 	ssGetPWork(S)[1] = (void*)psm;
 }
 //#define MDL_UPDATE
-//static void mdlUpdate(SimStruct *S, int_T tid)	// mdlUpdate() shouldn't be here as 
-//{													// we have direct feedthrough and 
-//	Filter *pf = (Filter*)ssGetPWork(S)[0];			// no discrete states
-//	double time = (double)ssGetT(S);				// We don't use state vectors either
+//static void mdlUpdate(SimStruct *S, int_T tid)    // mdlUpdate() shouldn't be here as 
+//{                                                 // we have direct feedthrough and 
+//	Filter *pf = (Filter*)ssGetPWork(S)[0];         // no discrete states
+//	double time = (double)ssGetT(S);                // We don't use state vectors either
 //	pf->ProjectAhead();
 //	pf->set_Time(time);
 //}
@@ -69,22 +69,20 @@ static void mdlOutputs(SimStruct *S, int_T task_id)
 	// Retrieving inputs:
 	InputRealPtrsType input = ssGetInputPortRealSignalPtrs(S, 0);
 	double inputs[2] = 
-	{ (double)(*input[0]) , (double)(*input[1]) };	// let's hope they are convertible
-	IMatrix *y = new Matrix<1, 1>;
-	y->at(0, 0) = inputs[0];						// measurement is first input
-	IMatrix *u = new Matrix<1, 1>;
-	u->at(0, 0) = inputs[1];						// reference signal is second input
+	{ (double)(*input[0]) , (double)(*input[1]) };  // let's hope they are convertible
+	Matrix y(1,1);
+	y.at(0, 0) = inputs[0];                         // measurement is first input
+	Matrix u(1, 1);
+	u.at(0, 0) = inputs[1];                         // reference signal is second input
 
 	// Step 2:
 	pf->UpdateEstimate(y);
-	delete y;
 
 	// Step 3:
 	pf->ComputeCovariance();
 
 	// Step 4:
 	pf->ProjectAhead(u);
-	delete u;
 
 	// Updating time:
 	double time = (double)ssGetT(S);
@@ -92,8 +90,8 @@ static void mdlOutputs(SimStruct *S, int_T task_id)
 
 	// Setting outputs:
 	real_T *output = ssGetOutputPortRealSignal(S, 0);
-	output[0] = (real_T)(pf->get_State(2));			// first output is psi
-	output[1] = (real_T)(pf->get_State(4));			// second output is b
+	output[0] = (real_T)(pf->get_State(2));         // first output is psi
+	output[1] = (real_T)(pf->get_State(4));         // second output is b
 }
 static void mdlTerminate(SimStruct *S)
 {
